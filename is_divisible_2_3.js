162f190b1d63d6ec7a260150f8dc7052fb440424
Anas Ibn Belal <98760910+anas20023@users.noreply.github.com>
2023-10-29 13:57:45 +0600

    is divisible? by 2 and 3

---
+++ is_divisible_2_3.js
@@ -0,0 +1,12 @@
+let a;
+a=prompt("Enter a Number To Check ");
+a=parseInt(a);
+
+if(a % 2 ==0 && a % 2 ==0)
+{
+    alert("The Number is Divisible by 2 and 3");
+}
+else
+{
+    alert("Not Divisible By 2 and 3");
+}
