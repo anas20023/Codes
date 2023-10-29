be0ed24d4ee81851d0cd2d3331480aa9d51a94d3
Anas Ibn Belal <98760910+anas20023@users.noreply.github.com>
2023-10-29 13:50:59 +0600

    switch_case

--- switch_case.js
+++ switch_case.js
@@ -1,2 +1,38 @@
-let a=prompt("Enter A Number");
-a=parseInt(a);
+let a; 
+a = prompt("Enter Your Number : ");
+a = parseInt(a);
+
+
+if (a > 10 && a < 0) {
+  alert("You Are Noy Luckey !!!!");
+} else {
+  switch (a) {
+    case 1:
+      alert("You Are Luckey !!!!");
+      break;
+    case 2:
+      alert("You Are Luckey !!!!");
+      break;
+    case 3:
+      alert("You Are Luckey !!!!");
+      break;
+    case 4:
+      alert("You Are Luckey !!!!");
+      break;
+    case 5:
+      alert("You Are Luckey !!!!");
+      break;
+    case 6:
+      alert("You Are Luckey !!!!");
+    case 7:
+      alert("You Are Luckey !!!!");
+      break;
+    case 8:
+      alert("You Are Luckey !!!!");
+      break;
+    case 9:
+      alert("You Are Luckey !!!!");
+    default:
+      alert("You Are Noy Luckey !!!!");
+  }
+}
