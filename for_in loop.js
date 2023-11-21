bc9406ef8edd067c8b0b91952c9e74e91c3fc586
anas20023 <yorder07@gmail.com>
2023-11-22 00:51:57 +0600

    Update script source file path

---
+++ for_in loop.js
@@ -0,0 +1,16 @@
+let student ={
+
+    name:"Anas Ibn Belal",
+    Dpt:"CSE",
+    id:372,
+    cgpa:3.92
+};
+
+
+// For in Loops for Object Type Data
+
+
+for(let key in student)
+{
+    alert(key + "  =  " + student[key] );
+}
