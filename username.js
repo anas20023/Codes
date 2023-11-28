604926f86636d640ec2796f86974ae517d61d76f
anas20023 <yorder07@gmail.com>
2023-11-28 09:09:07 +0600

    Refactor username generation code and update CSS styling

--- username.js
+++ username.js
@@ -1,12 +1,21 @@
-var fname,lwrc,nspc;
+var fname, lwrc, nspc;
 
 function Gettext() {
-   fname= document.getElementById("inp").value;
+  fname = document.getElementById("inp").value;
 }
 
 function convert_Name() {
-    lwrc = fname.toLowerCase();
-    nspc = lwrc.replaceAll(" ","");
-
-  document.getElementById("user_gen_T").innerHTML ="@" + nspc + nspc.length;
+  lwrc = fname.toLowerCase();
+  nspc = lwrc.replaceAll(" ", "");
+  if (nspc.length == 0) {
+    alert("Dont Leave the Field Empty !!");
+    document.getElementById("user_gen_T").innerHTML = "";
+  } else {
+    document.getElementById("user_gen_T").innerHTML =
+      "@" +
+      nspc +
+      Math.floor(Math.random() * 100 + 1) +
+      nspc.length +
+      Math.floor(Math.random() * 100 + 1);
+  }
 }
