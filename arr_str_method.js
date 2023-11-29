be4d7630a4d2aeee821c50db43835fa6bdb5c517
anas20023 <yorder07@gmail.com>
2023-11-29 17:47:46 +0600

    Fix bug in login functionality

---
+++ arr_str_method.js
@@ -0,0 +1,10 @@
+let company;
+company = ["Bloomberg" , "Microsoft" , "Uber" , "Google", "IBM","Netflix"];
+
+//company.shift(); // Shift remove the first elements from an array
+
+company.splice(2,1,"Ula"); // Splice will add 2th  Index's Uber and delete it and add Ola
+
+company.push("Amazon"); // It will Push Amazon at the End of the Array
+
+console.log(company);
