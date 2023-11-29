d8127889a71f137dbed45ee8c01505faf93a14e8
anas20023 <yorder07@gmail.com>
2023-11-29 17:55:15 +0600

    Remove first element from company array and replace "Uber" with "Ula"

--- arr_str_method.js
+++ arr_str_method.js
@@ -1,7 +1,7 @@
 let company;
 company = ["Bloomberg" , "Microsoft" , "Uber" , "Google", "IBM","Netflix"];
 
-//company.shift(); // Shift remove the first elements from an array
+company.shift(); // Shift remove the first elements from an array
 
 company.splice(2,1,"Ula"); // Splice will add 2th  Index's Uber and delete it and add Ola
 
