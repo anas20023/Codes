01708966838fae7890d224652bacab06f49d9fde
anas20023 <yorder07@gmail.com>
2023-12-23 23:09:37 +0600

    Refactor code and comment out unused sections

--- classes.js
+++ classes.js
@@ -9,21 +9,53 @@
 
 // Prototype Pass in Object
 
-let MyName = {
-  fname: "Anas Ibn Belal",
-  roll: 372,
-  class: "CSE",
-  Print_itms(){
-    alert('Hello World !!');
-  }
-};
+// let MyName = {
+//   fname: "Anas Ibn Belal",
+//   roll: 372,
+//   class: "CSE",
+//   Print_itms(){
+//     alert('Hello World !!');
+//   }
+// };
 
-let PrintInfo = {
-  Print_itms() {
-    console.log(this.fname);
-    console.log(this.roll);
-    console.log(this.class);
-  },
-};
+// let PrintInfo = {
+//   Print_itms() {
+//     console.log(this.fname);
+//     console.log(this.roll);
+//     console.log(this.class);
+//   },
+// };
 
-MyName.__proto__=PrintInfo;
+// MyName.__proto__=PrintInfo;
+
+//Class Defination
+
+// class CSE {
+//   constructor(name, roll, marks) {
+//     this.name = name;
+//     this.roll = roll;
+//     this.marks = marks;
+//   }
+//   CPGA(cgpa) {
+//     if (cgpa > 3.5) {
+//       console.log(`Your CGPA is = ${cgpa}`);
+//     } else {
+//       console.log("Your CGPA is too Low");
+//     }
+//   }
+// }
+
+
+// let Anas= new CSE("Anas",372,95);
+// console.log(Anas);
+// console.log(Anas.CPGA(3.95));
+// let Sifat=new CSE("Miel Mahmud Sifat",368,99);
+// console.log(Sifat);
+
+
+// Inheritation
+
+let Obj_1={
+
+
+}
