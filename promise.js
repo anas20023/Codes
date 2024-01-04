46d4cbffacc4400157a8c62cc62fca1b9b1f991a
anas20023 <yorder07@gmail.com>
2024-01-04 09:28:49 +0600

    Refactor promise.js file

--- promise.js
+++ promise.js
@@ -1,11 +1,25 @@
-let My_promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    alert("Sorry !!");
-    reject("Done !!");
-  }, 2000);
-});
+// let My_promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     alert("Sorry !!");
+//     reject("Done !!");
+//   }, 2000);
+// });
 
-console.log(My_promise);
-setTimeout(() => {
-  console.log(My_promise);
-}, 2000);
+// console.log(My_promise);
+// setTimeout(() => {
+//   console.log(My_promise);
+// }, 2000);
+
+function Print_Number(n) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Data of ${n}`);
+      resolve("ok");
+    }, 3000);
+  });
+}
+
+(async function () {
+  await Print_Number(10);
+  await Print_Number(11);
+})();
